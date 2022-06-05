3902b298903f8016424a3426b02f19783b7fe031
tmoino <tony.moino@student.moringaschool.com>
2022-06-05 18:20:06 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,26 @@
+function add (a, b){
+    return a + b;
+}
+function subtract(a ,b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b ;
+}
+function increment (n){
+    let a = ++n;
+    return a ;
+}
+function decrement(n) {
+    let a = --n;
+    return a ;
+}
+function makeInt(String) {
+    return parseInt(String,10);
+}
+function preserveDecimal(String){
+    return parseFloat(String)
+}
